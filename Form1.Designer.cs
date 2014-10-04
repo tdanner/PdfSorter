6036f26df5e97aeed07cc03e93c819cba4a9bcc9
Tim Danner <tim@danners.us>
2014-10-04 17:09:32 -0500

    Initial commit. Barely works.

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace PdfSorter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.moveButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(624, 552);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.moveButton);
+            this.splitContainer1.Panel1.Controls.Add(this.textBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.webBrowser1);
+            this.splitContainer1.Size = new System.Drawing.Size(624, 603);
+            this.splitContainer1.SplitterDistance = 47;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // moveButton
+            // 
+            this.moveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.moveButton.Location = new System.Drawing.Point(537, 10);
+            this.moveButton.Name = "moveButton";
+            this.moveButton.Size = new System.Drawing.Size(75, 23);
+            this.moveButton.TabIndex = 1;
+            this.moveButton.Text = "Move";
+            this.moveButton.UseVisualStyleBackColor = true;
+            this.moveButton.Click += new System.EventHandler(this.moveButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(519, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.moveButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 603);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button moveButton;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace PdfSorter
+{
+    public partial class Form1 : Form
+    {
+        const string Path = @"C:\Users\Tim\Dropbox\My ScanSnap\";
+
+        private string _typeAhead = string.Empty;
+        private readonly HashSet<string> _candidates;
+        private string _currentFile;
+        private string _targetPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _candidates = new HashSet<string>(Directory.GetDirectories(Path, "*", SearchOption.AllDirectories).Select(d => d.Substring(Path.Length)),
+                StringComparer.OrdinalIgnoreCase);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ShowNextFile();
+        }
+
+        private void ShowNextFile()
+        {
+            var files = Directory.GetFiles(Path, "*.pdf");
+            _currentFile = files[0];
+            webBrowser1.Navigate(_currentFile);
+        }
+
+        private void FindBestMatch()
+        {
+            string bestMatch = _candidates.OrderBy(p => MatchQuality(p, _typeAhead)).First();
+            textBox1.Text = bestMatch;
+            _targetPath = System.IO.Path.Combine(Path, bestMatch);
+        }
+
+        private static int MatchQuality(string str, string pattern)
+        {
+            int matchIndex = str.IndexOf(pattern, StringComparison.OrdinalIgnoreCase);
+            if (matchIndex < 0)
+                matchIndex = int.MaxValue;
+            return matchIndex;
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+            _typeAhead += e.KeyChar;
+            FindBestMatch();
+        }
+
+        private void moveButton_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("about:blank");
+            while (webBrowser1.ReadyState != WebBrowserReadyState.Complete)
+            {
+                Application.DoEvents();
+                Thread.Sleep(TimeSpan.FromMilliseconds(100));
+            }
+            File.Move(_currentFile, System.IO.Path.Combine(_targetPath, System.IO.Path.GetFileName(_currentFile)));
+            ShowNextFile();
+            _typeAhead = string.Empty;
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                e.Handled = true;
+                _typeAhead = string.Empty;
+                _targetPath = string.Empty;
+                textBox1.Text = string.Empty;
+            }
+        }
+    }
+}
